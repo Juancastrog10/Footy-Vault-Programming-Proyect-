c836a3f30430fad8da00c5837310bbba5d814ba3
Juancastrog10 <juancastrogrimaldi10@gmail.com>
2024-05-28 19:58:34 -0500

    arreglo descripción

--- Main Page/des.js
+++ Main Page/des.js
@@ -3,8 +3,8 @@ import { cargarInformacion, Producto } from "./desutils.js";
 const mostrarDetalleProducto = async () => {
     try {
         const urlParams = new URLSearchParams(window.location.search);
-        const productId = urlParams.get('id');
-        console.log('ID del producto:', productId);
+        const productId = Number(urlParams.get('id')); // Convertir a número
+        console.log('ID del producto en URL:', productId);
 
         if (!productId) {
             console.error('ID del producto no proporcionado en la URL');
@@ -12,14 +12,16 @@ const mostrarDetalleProducto = async () => {
         }
 
         const data = await cargarInformacion();
-        console.log('Lista de productos:', data.familiadeproductos);
+        console.log('Datos cargados del JSON:', data);
 
         const productos = data.familiadeproductos.map(item => new Producto(item));
+        console.log('Productos después de mapear:', productos);
+
         const producto = productos.find(item => item.id === productId);
+        console.log('Producto encontrado:', producto);
 
         if (producto) {
-            console.log('Producto encontrado:', producto);
-            producto.render();
+            producto.renderDetalle();
         } else {
             console.error('Producto no encontrado en la lista de productos');
         }
